8fd613f78a1415452bfeea547881c9ac84715b91
Reid Herrera <reidherrerabusiness@gmail.com>
2025-10-02 18:59:28 -0500

    completed boiler plate

--- Program.cs
+++ Program.cs
@@ -1,2 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("What we need to maintain is critical boost 5, weakness exploit 5, maximum might3, antivirus would be nice 3, self improveis cool.");
+
+Console.ReadKey();
+
+public class Helmet
+{
+  void HelmetDescription()
+  {
+    System.Console.WriteLine("Burst 2");
+    System.Console.WriteLine("only 1 deco medium slot");
+  }
+}
+
+public class Chest
+{
+  public void ChestDescription()
+  {
+    System.Console.WriteLine("Evade Window 2");
+    System.Console.WriteLine("1 large and 1 small deco");
+  }
+
+  public void NewChestDescription()
+  {
+    System.Console.WriteLine("");
+    System.Console.WriteLine("");
+  }
+}
+
+public class Arms
+{
+
+  public void ArmsDescription()
+  {
+    System.Console.WriteLine("Consitution + Evade window 1 each");
+    System.Console.WriteLine("Two medium decorations");
+  }
+  public void NewChestDescription()
+  {
+    System.Console.WriteLine("");
+    System.Console.WriteLine("");
+  }
+}
+
+public class Belt
+{
+  public void BeltDescription()
+  {
+    System.Console.WriteLine("Constitution 2");
+    System.Console.WriteLine("1 large and 1 medium deco");
+  }
+
+    public void NewChestDescription()
+  {
+    System.Console.WriteLine("");
+    System.Console.WriteLine("");
+  }
+}
+
+public class Legs
+{
+  public void LegsDescription()
+  {
+    System.Console.WriteLine("Antivirus and Flinch free 1");
+    System.Console.WriteLine("1 large, two small.");
+  }
+
+    public void NewChestDescription()
+  {
+    System.Console.WriteLine("");
+    System.Console.WriteLine("");
+  }
+}
